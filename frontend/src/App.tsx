a80afa5e42ed7c1f4152c937f5c866cf5cd9ed17
AhmetUtkuPelen <aup.aup.3548@gmail.com>
2025-03-17 20:02:46 +0300

    fe setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,29 @@
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Pages/Home/Home"
+import Register from "./Pages/Register/Register"
+import Login from "./Pages/Login/Login"
+import Settings from "./Pages/Settings/Settings"
+import Profile from "./Pages/Profile/Profile"
 
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <div>
+      
+      <Navbar/>
+      
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    
+    </div>
   )
 }
 
